55e6acba2bb7804346438eb9bbecf68f3c13e326
Matt Reyes Ben Logue <none@none.com>
2016-09-26 16:35:42 -0700

    add initial files and youtube api call to get videos based on a search query

---
+++ src/YoutubeAPITest/Models/Search.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace YoutubeAPITest.Models
+{
+    /// <summary>
+    /// YouTube Data API v3 sample: search by keyword.
+    /// Relies on the Google APIs Client Library for .NET, v1.7.0 or higher.
+    /// See https://code.google.com/p/google-api-dotnet-client/wiki/GettingStarted
+    ///
+    /// Set ApiKey to the API key value from the APIs & auth > Registered apps tab of
+    ///   https://cloud.google.com/console
+    /// Please ensure that you have enabled the YouTube Data API for your project.
+    /// </summary>
+    public class Search
+    {
+        public Search() { }
+        public static void TestSearch() { 
+            Console.WriteLine("YouTube Data API: Search");
+            Console.WriteLine("========================");
+
+            try
+            {
+                new Search().Run().Wait();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public async Task<List<Video>> Run()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyBdVIEOHVhYVD2shSFPKpiTlaKmjrT9FU8",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = "Jiu Jitsu"; // Replace with your search term.
+            searchListRequest.MaxResults = 50;
+
+            // Call the search.list method to retrieve results matching the specified query term.
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            List<Video> videos = new List<Video>();
+            List<string> channels = new List<string>();
+            List<string> playlists = new List<string>();
+            
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        videos.Add(new Video(searchResult.Snippet.Title, searchResult.Id.VideoId, searchResult.Snippet.ChannelTitle, searchResult.Snippet.Description, searchResult.Snippet.Thumbnails.Default__.Url));
+                        break;
+
+                    case "youtube#channel":
+                        channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
+                        break;
+
+                    case "youtube#playlist":
+                        playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
+                        break;
+                }
+            }
+            Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
+            Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
+            Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
+            return videos;
+        }
+    }
+}
---
+++ src/YoutubeAPITest/Models/Video.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoutubeAPITest.Models
+{
+    public class Video
+    {
+        public string Title { get; set; } 
+        public string VideoId { get; set; }
+        public string ChannelName { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public Video(string title, string videoId, string channelName, string description, string image)
+        {
+            Title = title;
+            VideoId = videoId;
+            ChannelName = channelName;
+            Description = description;
+            Image = image;
+        }
+    }
+}
